3564c8700a6c9ca2b6373bd5700c3b0f81a92ecf
werzet <10843461+Werzet@users.noreply.github.com>
2023-05-30 21:24:11 +1000

    Добавил начальные файлы

---
+++ WatchdogOrchestraClient/Configuration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WatchdogOrchestraClient;
+
+internal class Configuration
+{
+	public string Address { get; set; } = string.Empty;
+
+	public string Token { get; set; } = string.Empty;
+}
---
+++ WatchdogOrchestraClient/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+using Spectre.Console;
+using WatchdogOrchestraClient;
+
+bool endApp = false;
+
+AnsiConsole.WriteLine("[blue] SS14 watchdog orchestrat client [/]");
+
+var configText = File.ReadAllBytes("config.json");
+
+var config = JsonSerializer.Deserialize<Configuration>(configText) ?? new Configuration();
+
+if (string.IsNullOrWhiteSpace(config.Address))
+{
+	config.Address = AnsiConsole.Ask<string>("Введите адрес сервера");
+}
+
+if (string.IsNullOrWhiteSpace(config.Token))
+{
+
+}

d314624fecb754d056cda1d63f6d4c6d6875266a
werzet <10843461+Werzet@users.noreply.github.com>
2023-05-31 17:02:34 +1000

    Добавил исполнение команд

---
+++ WatchdogOrchestraClient/ConfigFileManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+using System.Text.Json;
+
+namespace WatchdogOrchestraClient;
+
+internal class ConfigFileManager
+{
+	private static string ConfigName => "config.json";
+
+	public static Configuration GetConfig()
+	{
+		var configText = File.ReadAllBytes(ConfigName);
+
+		return JsonSerializer.Deserialize<Configuration>(Encoding.UTF8.GetString(configText), new JsonSerializerOptions() { PropertyNameCaseInsensitive = true }) ?? new Configuration();
+	}
+
+	internal static void SaveConfig(Configuration config)
+	{
+		var jConfig = JsonSerializer.Serialize(config, new JsonSerializerOptions() { WriteIndented = true });
+
+		File.WriteAllBytes(ConfigName, Encoding.UTF8.GetBytes(jConfig));
+	}
+}
---
+++ WatchdogOrchestraClient/ExceptionHandler.cs
@@ -0,0 +1,31 @@
+﻿using Spectre.Console;
+
+namespace WatchdogOrchestraClient;
+
+internal static class ExceptionHandler
+{
+	public static void Init()
+	{
+		AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+	}
+
+	private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+	{
+		if (e.ExceptionObject is Exception exc)
+		{
+			HandleError("Необработанная ошибка", exc);
+		}
+		else
+		{
+			HandleError("Необработанная ошибка", new Exception(e.ExceptionObject.ToString()));
+		}
+	}
+
+	public static void HandleError(string msg, Exception exc)
+	{
+		AnsiConsole.WriteLine();
+		AnsiConsole.Write(new Rule(msg).LeftJustified());
+		AnsiConsole.WriteLine();
+		AnsiConsole.WriteException(exc);
+	}
+}
